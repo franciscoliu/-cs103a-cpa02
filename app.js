64e3d126d93abd9419c8c5841b9d54c3d646c8e0
franciscoliu <zheyuanliu@brandeis.edu>
2022-04-23 14:57:49 -0400

    temp

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const client = new OAuth2Client(CLIENT_ID);
 
 const mongoose = require( 'mongoose' );
 // const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
-const mongodb_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rdpsv.mongodb.net/Project?retryWrites=true&w=majority`
+// const mongodb_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rdpsv.mongodb.net/Project?retryWrites=true&w=majority`
+const mongodb_URI = 'mongodb+srv://zheyuanliu:140!XuzuPe0106@cluster0.rdpsv.mongodb.net/Project?retryWrites=true&w=majority'
 
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 // fix deprecation warnings
